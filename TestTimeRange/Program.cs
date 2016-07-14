14b042023abb61328787eeba5134c3a98799c7af
Alexey <alex.v.timofeev@gmail.com>
2016-07-15 00:29:40 +0500

    Добавьте файлы проекта.

---
+++ TestTimeRange/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTimeRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeRange test = new TimeRange();
+
+            test.Update(TimeSpan.FromSeconds(10));
+            test.Update(TimeSpan.FromSeconds(6));
+            test.Update(TimeSpan.FromSeconds(3));
+            test.Update(TimeSpan.FromSeconds(12));
+            test.Update(TimeSpan.FromSeconds(4));
+            test.Update(TimeSpan.FromSeconds(3));
+            test.Update(TimeSpan.FromSeconds(8));
+        }
+    }
+}
---
+++ TestTimeRange/TimeRange.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTimeRange
+{
+    public class TimeRange
+    {
+        public TimeSpan Min { get; protected set; }
+
+        public TimeSpan Max { get; protected set; }
+
+        public void Update(TimeSpan source)
+        {
+            if (Min == TimeSpan.Zero || (Min > source && source < Max))
+            {
+                Min = source;
+            }
+            if (Max == TimeSpan.Zero || (Max < source && source > Min))
+            {
+                Max = source;
+            }
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} - {1}", Min, Max);
+        }
+    }
+}
